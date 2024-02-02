c5fc2a11be42e36145deb8cd416085c86e2516d0
kepo402 <alawodeolayinka2@gmail.com>
2024-02-02 23:07:14 +0100

    made migrations for the app

--- football_app/models.py
+++ football_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Tournament(models.Model):
+    name = models.CharField(max_length=100)
+    start_date = models.DateField()
+    end_date = models.DateField()
+
+class Team(models.Model):
+    name = models.CharField(max_length=100)
+    tournament = models.ForeignKey(Tournament, on_delete=models.CASCADE)
+
+class Player(models.Model):
+    name = models.CharField(max_length=100)
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    goals_scored = models.IntegerField()
+    assist = models.IntegerField()        
--- football_app/views.py
+++ football_app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Tournament, Team, Player
 
 # Create your views here.
+def tournament_list(request):
+    tournaments = Tournament.objects.all()
+    return render(request, 'football_app/tournament_list.html', {'tournaments': tournaments})
+
+
+def team_detail(request, team_id):
+    team = Team.objects.get(pk=team_id)
+    players = Player.objects.filter(team=team)
+    return render(request, 'football_app/team_detail.html', {'team': team, 'players': players})
+
