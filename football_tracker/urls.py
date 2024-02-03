6fd907e00c0c1a39abf361a7e432759d75463349
kepo402 <alawodeolayinka2@gmail.com>
2024-02-03 06:38:30 +0100

    added a base html

--- football_tracker/urls.py
+++ football_tracker/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('football/', include('football_app.urls')),
+    path('', RedirectView.as_view(url='/football/tournaments/', permanent=True)),
 ]
